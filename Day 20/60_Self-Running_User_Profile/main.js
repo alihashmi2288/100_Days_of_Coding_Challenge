1c84d41bc696ac9107945c933d0765e9ade2657e
Syed Ali Hashmi <160578567+alihashmi2288@users.noreply.github.com>
2024-03-21 12:29:24 +0500

    Update main.js

--- Day 20/60_Self-Running_User_Profile/main.js
+++ Day 20/60_Self-Running_User_Profile/main.js
@@ -1,7 +1,7 @@
 //Question 60: Self-Running User Profile: Create a quick, self-setup profile for a user that can tell you the user's name and age.
 var profile = (function () {
     //User profile
-    var name = "Ahmed Raza";
+    var name = "Syed Ali Hashmi";
     var age = 27;
     //shared profile part
     return {
