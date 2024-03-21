20247e6db7153ebb30db42b2a6a8ff72b4f0d07d
Syed Ali Hashmi <160578567+alihashmi2288@users.noreply.github.com>
2024-03-21 12:24:19 +0500

    Update main.js

--- Day 01/2_Personal_Message/main.js
+++ Day 01/2_Personal_Message/main.js
@@ -1,4 +1,4 @@
 // store name in a variable
-var personName = "Ahmed Raza";
+var personName = "Syed Ali Hashmi";
 //print a message using the assigned variable
 console.log("Hello " + personName + ", would you like to learn some pythone today?");
